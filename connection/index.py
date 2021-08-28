4d8ae125c0187ba24d9b14164f38f46dfc309d4f
Michelle <mcorleone1946@gmail.com>
2021-08-28 00:52:39 -0500

    Implementation of Classes

--- connection/index.py
+++ connection/index.py
@@ -1,14 +1,14 @@
 #authors Michelle Quintero Hernandez
-# import webdriver
+
 from selenium import webdriver
-#from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from model.Car import Car
 
 #Search elements declarations
-car = "renault-4"
+car = "renault-12"
 indexNum = 0
 index = "Desde_"+str(48*indexNum+1)
 sort = "OrderId_PRICE"
-pRange = [0,10000000]
+pRange = [0,12000000]
 priceRange = "PriceRange_"+str(pRange[0])+"-"+str(pRange[1])
 yRange = ["hasta","1992"]
 yearRange = str(yRange[0])+"-"+str(yRange[1])+"/"
@@ -22,5 +22,17 @@ driver.get("https://carros.mercadolibre.com.co/"+yearRange+car+"_"+index+"_"+sor
 #get results
 results = driver.find_elements_by_class_name("ui-search-layout__item")
 
+#Adding results
+cars = list()
 for r in results:
-    print("si")
+    price = int(r.find_element_by_class_name("price-tag-fraction").text.replace(".",""))
+    title = r.find_element_by_class_name("ui-search-item__title ui-search-item__group__element").text
+    location = r.find_element_by_class_name("ui-search-item__group__element ui-search-item__location").text
+    year = r.find_element_by_class_name("ui-search-card-attributes__attribute").text
+
+    car = Car(price,title,location,year)
+    cars.append(car)
+
+print(cars)
+
+    
---
+++ model/Car.py
@@ -0,0 +1,11 @@
+class Car:
+    price = 0
+    title = ""
+    location = ""
+    year = ""
+
+    def __init__(self, price, title, location, year):
+        self.price = price
+        self.title = title
+        self.location = location
+        self.year = year
